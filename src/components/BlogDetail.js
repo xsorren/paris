5940b6a0ee9e80a3e12106c9eb391bdfa0c8d592
xsorren <lauta.sorren@gmail.com>
2025-10-11 19:59:59 -0300

    a

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
