e558b0a922bc9e9ec84dcd45065f948ac20095ee
FacundoPeraltaa <facuperalta1234@gmail.com>
2026-05-09 18:24:35 -0300

    PRUEBA HOSTING FIREBASE

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ignora ESLint solo en builds (Netlify)
-  },
-};
-module.exports = nextConfig;
